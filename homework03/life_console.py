bfaeac08f5ae3ad14eadf266b4e07a688ce9193c
BortkoD <megymikei@gmail.com>
2021-03-19 11:47:50 +0300

    Done without arguments

--- homework03/life_console.py
+++ homework03/life_console.py
@@ -1,42 +1,48 @@
 import curses
+import time
 
 from life import GameOfLife
 from ui import UI
 
 
 class Console(UI):
-    def __init__(self, life: GameOfLife) -> None:
+    def __init__(self, life: GameOfLife, speed = 1) -> None:
         super().__init__(life)
+        self.life.curr_generation = self.life.create_grid(True)
+        self.speed = speed
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        screen.box()
+        curses.resizeterm(self.life.rows + 2, self.life.cols + 2)
+        screen.border()
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        grid = self.life.curr_generation
         for y in range(self.life.rows):
             for x in range(self.life.cols):
-                if grid[y][x] == 1:
-                    screen.addch(y, x, '~')
+                if self.life.curr_generation[y][x] == 1:
+                    screen.addstr(y+1, x+1, '*')
                 else:
-                    screen.addch(y, x, ' ')
+                    screen.addstr(y+1, x+1, ' ')
 
     def run(self) -> None:
-        curses.initscr()
-        win = curses.newwin(self.life.rows, self.life.cols, 0, 0)
+        screen = curses.initscr()
+        self.draw_borders(screen)
+        curses.curs_set(0)
+        screen.refresh()
         # PUT YOUR CODE HERE
         running = True
         while running:
-            self.draw_borders(win)
-#            self.draw_grid(screen)
+            screen.clear()
             self.life.step()
-            win.clear()
-            win.refresh()
+            self.draw_borders(screen)
+            self.draw_grid(screen)
+            screen.refresh()
+            time.sleep(self.speed)
         curses.endwin()
 
 
 if __name__ == "__main__":
-    gol = GameOfLife((240, 120))
+    gol = GameOfLife((50, 50))
     ui = Console(gol)
     ui.run()
