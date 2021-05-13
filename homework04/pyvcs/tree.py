fe4c35b1b8f5c1ac7c8fe8166a80e71de1ef9c49
BortkoD <megymikei@gmail.com>
2021-05-13 19:44:28 +0300

    v5

--- homework04/pyvcs/tree.py
+++ homework04/pyvcs/tree.py
@@ -11,8 +11,23 @@ from pyvcs.refs import get_ref, is_detached, resolve_head, update_ref
 
 def write_tree(gitdir: pathlib.Path, index: tp.List[GitIndexEntry], dirname: str = "") -> str:
     # PUT YOUR CODE HERE
-    ...
-
+    content = b""
+    for entry in index:
+        if "/" in entry.name:
+            dir_name = entry.name[:entry.name.find("/")]
+            child_content = oct(entry.mode)[2:].encode() + b" "
+            child_content += entry.name[entry.name.find("/") + 1:].encode() + b"\0"
+            child_content += entry.sha1
+            mode = b"40000 "
+            name = dir_name.encode() + b"\0"
+            sha1 = bytes.fromhex(hash_object(child_content, "tree", True))
+        else:
+            mode = oct(entry.mode)[2:].encode() + b" "
+            name = entry.name.encode() + b"\0"
+            sha1 = entry.sha1
+        content += mode + name + sha1
+    result = hash_object(content, "tree", write=True)
+    return result
 
 def commit_tree(
     gitdir: pathlib.Path,
